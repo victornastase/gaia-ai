5c30f6e9f4c422dd472818926bfb037ec347dcc7
nastasevictor <victor.gabriel.nastase@gmail.com>
2024-08-30 16:26:00 +0300

    format the response from gpt

--- src/main/java/com/singularity/god/gaia/resources/TalkToGaia.java
+++ src/main/java/com/singularity/god/gaia/resources/TalkToGaia.java
@@ -1,13 +1,13 @@
 package com.singularity.god.gaia.resources;
 
 import com.singularity.god.gaia.models.AskGAIA;
-import com.singularity.god.gaia.models.MessageFromGAIA;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.ChatClient;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
@@ -25,9 +25,9 @@ public class TalkToGaia {
             summary = "Here you can talk to future gods",
             description = "Don't worry! GAIA assistant will add the politeness to your message! Just ask ... and pray"
     )
-    @PostMapping("/prompt")
-    public MessageFromGAIA giveMeWisdom(@Valid @RequestBody final AskGAIA myMessageForTheGod) {
-        return new MessageFromGAIA(core.call(myMessageForTheGod.getQuestion()));
+    @PostMapping(value = "/prompt", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String giveMeWisdom(@Valid @RequestBody final AskGAIA myMessageForTheGod) {
+        return core.call(myMessageForTheGod.getQuestion());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
